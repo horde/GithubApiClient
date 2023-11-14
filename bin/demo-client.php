7775a8f8f47d55ab9d122379f584d3aad110920f
Ralf Lang <ralf.lang@ralf-lang.de>
2023-11-14 15:47:44 +0100

    Make the demo client more robust

--- bin/demo-client.php
+++ bin/demo-client.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
 namespace Horde\GithubApiClient;
 
 // Development Reference cli
-require_once '../vendor/autoload.php';
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 
 use Horde\Injector\Injector;
@@ -21,6 +21,10 @@ use Psr\Http\Message\RequestFactoryInterface;
 
 // Bootstrap the injector
 $strGithubApiToken = (string) getenv('GITHUB_TOKEN');
+if (empty($strGithubApiToken)) {
+    echo "\nNo can do. Did you forget to export GITHUB_TOKEN variable?\n";
+    exit;
+}
 $injector = new Injector(new TopLevel());
 // Setup a curl client. This is a demo, don't get too involved
 $injector->setInstance(ClientInterface::class, new CurlClient(new ResponseFactory(), new StreamFactory(), new Options()));
@@ -28,6 +32,5 @@ $injector->setInstance(RequestFactoryInterface::class, new RequestFactory());
 $injector->setInstance(GithubApiConfig::class, new GithubApiConfig(accessToken: $strGithubApiToken));
 $client = $injector->get(GithubApiClient::class);
 $repos = $client->listRepositoriesInOrganization(new GithubOrganizationId('horde'));
-print_r($repos);
 
 // List Releases of a repo
